5c82466b0951f0c23a64ff9b9c8aeab9a6bd02ff
Jeff Fortune <fortunej3ff@gmail.com>
2020-04-04 02:14:59 -0400

    Modularized App Color

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
           // or simply save your changes to "hot reload" in a Flutter IDE).
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
-          primarySwatch: Colors.yellow),
+          primaryColor: Color(0xFFFFDD28)),
       home: MyHomePage(title: 'Tüter'),
     );
   }
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      backgroundColor: Color(0xFFFFDD28),
+      backgroundColor: Theme.of(context).primaryColor,
     );
   }
 }
